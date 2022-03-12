d827532041653625d57fc3638334d4a6ce674492
Glitch (water-hip-parrot) <none>
2022-03-12 03:13:10 +0000

    🦣😯 Updated with Glitch

--- README.md
+++ README.md
@@ -1,18 +1,30 @@
-# Pre-work - *Memory Game*
+# Hello website!
 
-**Memory Game** is a Light & Sound Memory game to apply for CodePath's SITE Program. 
+This is a basic HTML starter project you can build on however you like. No need to save. While you develop your site, your changes will happen ✨ immediately in the preview window. On the left you'll see the files that make up your site, including HTML, JavaScript, and CSS. You can upload assets like images or audio in `assets`. The rest is up to you and your imagination. 🦄
 
-Submitted by: **Lazizbek Ravshanov**
+## What's in this project?
 
-Time spent: **7** hours spent in total
+← `README.md`: That's this file, where you can tell people what your cool website does and how you built it.
 
-# Deliverables:
-**There will be five (5) tasks for this pre-work assignment:**
+← `index.html`: This is the main web page for your site. The HTML defines the structure and content of the page using _elements_. You'll see references in the HTML to the JS and CSS files. Try clicking the image in the center of the page!
 
-1. Create a light and sound memory game
-2. Record a gif of all the implemented features
-3. Reflections about the light and sound memory game
-4. Interview Recording
-5. Submit your work on your application dashboard
+← `style.css`: CSS files add styling rules to your content. The CSS applies styles to the elements in your HTML page. The style rules also make the image move when you click it.
 
-The pre-work assignment will only be considered complete when all tasks listed above have been accomplished.
+← `script.js`: If you're feeling fancy you can add interactivity to your site with JavaScript. The code in the JavaScript file runs when the page loads, and when the visitor clicks the button you can add below.
+
+Open each file and check out the comments (in gray) for more info.
+
+## Try this next 🏗️
+
+Take a look in `TODO.md` for next steps you can try out in your new site!
+
+___Want a minimal version of this project to build your own website? Check out [Blank Website](https://glitch.com/edit/#!/remix/glitch-blank-website)!___
+
+![Glitch](https://cdn.glitch.com/a9975ea6-8949-4bab-addb-8a95021dc2da%2FLogo_Color.svg?v=1602781328576)
+
+## You built this with Glitch!
+
+[Glitch](https://glitch.com) is a friendly community where millions of people come together to build web apps and websites.
+
+- Need more help? [Check out our Help Center](https://help.glitch.com/) for answers to any common questions.
+- Ready to make it official? [Become a paid Glitch member](https://glitch.com/pricing) to boost your app with private sharing, more storage and memory, domains and more.
---
+++ script.js
@@ -0,0 +1,140 @@
+//Global constants
+const clueHoldTime = 1000; //how long to hold each clue's light/sound
+const cluePauseTime = 333; //how long to pause in between clues
+const nextClueWaitTime = 1000; //how long to wait before starting playback of the clue sequence
+
+//Global Variables
+var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
+var progress = 0;
+var gamePlaying = false;
+var tonePlaying = false;
+var volume = 0.5; //must be between 0.0 and 1.0
+var guessCounter = 0;
+
+function startGame() {
+    //initialize game variables
+    progress = 0;
+    gamePlaying = true;
+    // swap the Start and Stop buttons
+    document.getElementById("startBtn").classList.add("hidden");
+    document.getElementById("stopBtn").classList.remove("hidden");
+    playClueSequence();
+}
+
+function stopGame() {
+    gamePlaying = false;
+    // swap the Start and Stop buttons
+    document.getElementById("startBtn").classList.remove("hidden");
+    document.getElementById("stopBtn").classList.add("hidden");
+}
+
+// Sound Synthesis Functions
+const freqMap = {
+    1: 261.6,
+    2: 329.6,
+    3: 392,
+    4: 466.2
+}
+
+function playTone(btn, len) {
+    o.frequency.value = freqMap[btn]
+    g.gain.setTargetAtTime(volume, context.currentTime + 0.05, 0.025)
+    context.resume()
+    tonePlaying = true
+    setTimeout(function() {
+        stopTone()
+    }, len)
+}
+
+function startTone(btn) {
+    if (!tonePlaying) {
+        context.resume()
+        o.frequency.value = freqMap[btn]
+        g.gain.setTargetAtTime(volume, context.currentTime + 0.05, 0.025)
+        context.resume()
+        tonePlaying = true
+    }
+}
+
+function stopTone() {
+    g.gain.setTargetAtTime(0, context.currentTime + 0.05, 0.025)
+    tonePlaying = false
+}
+
+// Page Initialization
+// Init Sound Synthesizer
+var AudioContext = window.AudioContext || window.webkitAudioContext
+var context = new AudioContext()
+var o = context.createOscillator()
+var g = context.createGain()
+g.connect(context.destination)
+g.gain.setValueAtTime(0, context.currentTime)
+o.connect(g)
+o.start(0)
+
+function lightButton(btn) {
+    document.getElementById("button" + btn).classList.add("lit")
+}
+
+function clearButton(btn) {
+    document.getElementById("button" + btn).classList.remove("lit")
+}
+
+function playSingleClue(btn) {
+    if (gamePlaying) {
+        lightButton(btn);
+        playTone(btn, clueHoldTime);
+        setTimeout(clearButton, clueHoldTime, btn);
+    }
+}
+
+function playClueSequence() {
+    guessCounter = 0;
+    let delay = nextClueWaitTime; //set delay to initial wait time
+    for (let i = 0; i <= progress; i++) { // for each clue that is revealed so far
+        console.log("play single clue: " + pattern[i] + " in " + delay + "ms")
+        setTimeout(playSingleClue, delay, pattern[i]) // set a timeout to play that clue
+        delay += clueHoldTime
+        delay += cluePauseTime;
+    }
+}
+
+function loseGame() {
+    stopGame();
+    alert("Game Over. You lost.");
+}
+
+function winGame() {
+    stopGame();
+    alert("Game Over. You did it!");
+}
+
+function guess(btn) {
+    console.log("user guessed: " + btn);
+    if (!gamePlaying) {
+        return;
+    }
+
+    if (pattern[guessCounter] == btn) {
+        //guess was correct
+        if (guessCounter == progress) {
+            if (progress == pattern.length - 1) {
+                //gameover, user wins
+                winGame();
+            } else {
+                //pattern correct. Add next segment
+                progress++;
+                playClueSequence();
+            }
+        } else {
+            //check the next guess
+            guessCounter++;
+        }
+    } else {
+        //guess was incorrect
+        //gameover, user loses
+        loseGame();
+    }
+
+    // add game logic here
+}
